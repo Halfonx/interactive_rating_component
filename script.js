d86f77c564394e534f88ca368eb726af788601b3
Jérémy <105813902+Halfonx@users.noreply.github.com>
2022-07-21 18:51:46 +0200

    Add files via upload
    Creating HTML, CSS and JavaScript for the exercise

---
+++ script.js
@@ -0,0 +1,24 @@
+let ratings = document.querySelectorAll(".rating_list_item");
+let rateSpan = document.getElementById("rate_span");
+let submitButton = document.getElementsByClassName("rating_card_button");
+let landingCard = document.querySelector(".rating_card_landing");
+let submittedCard = document.querySelector(".rating_card_submitted");
+
+for (let i = 0; i < ratings.length; i++) {
+    ratings[i].addEventListener('click', () => {
+        let rating = ratings[i].innerHTML;
+
+        for (let i = 0; i < ratings.length; i++) {
+            ratings[i].classList.contains("selected");
+            ratings[i].classList.remove("selected");
+        }
+        ratings[i].classList.add('selected');
+
+        submitButton[0].addEventListener('click', () => {
+            landingCard.style.display = "none";
+            submittedCard.style.display = "block";
+        })
+
+        rateSpan.innerHTML = rating;
+    });
+}
